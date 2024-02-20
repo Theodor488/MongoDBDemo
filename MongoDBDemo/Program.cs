1a58c166cb83234510fa927dc155ae50990f685f
Theodor Suciu <theodorsuciu@yahoo.com>
2024-02-20 09:27:34 -0800

    init connection

--- MongoDBDemo/Program.cs
+++ MongoDBDemo/Program.cs
@@ -1,10 +1,23 @@
 ﻿
 using MongoDB.Driver;
-using MongoDBDemoApp;
+using MongoDBDemo;
 
+string connectionString = "mongodb://127.0.0.1:27017";
+string databaseName = "simple_db";
+string collectionName = "people";
 
+var client = new MongoClient(connectionString);
+var db = client.GetDatabase(databaseName);
+var collection = db.GetCollection<PersonModel>(collectionName);
 
+var person = new PersonModel { FirstName = "Theo", LastName = "Suciu" };
 
+await collection.InsertOneAsync(person);
 
+var results = await collection.FindAsync(_ => true);
 
+foreach (var result in results.ToList())
+{
+    Console.WriteLine($"{result.Id}: {result.FirstName} {result.LastName}");
+}
 
